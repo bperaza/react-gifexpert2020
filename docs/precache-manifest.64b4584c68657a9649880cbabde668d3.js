adc8c093947f4d8b25b441e271cfd641f504d9d8
bperaza <bernardo_peraza@hotmail.com>
2020-09-29 15:00:23 -0500

    Primer Commit

---
+++ docs/precache-manifest.64b4584c68657a9649880cbabde668d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a88260cfbc4ed12da95c9a1cf7604ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98ee9333063d7b2716c1",
+    "url": "/static/css/main.4c4c818d.chunk.css"
+  },
+  {
+    "revision": "496f528a92b3c1e745d3",
+    "url": "/static/js/2.6d4f56ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d4f56ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ee9333063d7b2716c1",
+    "url": "/static/js/main.98818dc3.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
